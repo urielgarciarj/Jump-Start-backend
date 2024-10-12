dbec970309973f5a0cfea42616b40b18520a9fb4
Uriel <urielgarciarj123@gmail.com>
2024-10-12 12:55:56 -0600

    Endpoint register first version
    Signed-off-by: Uriel <urielgarciarj123@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'jump-start-db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/user.entity.ts
@@ -0,0 +1,18 @@
+import { Entity, Column, PrimaryGeneratedColumn, BaseEntity } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  password: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  // Rol
+}
