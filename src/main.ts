888731e8c10b01c6875fd646cdb8370abcf5eb56
Vianey Nava <vianey.nava@bringitps.com>
2024-10-14 08:10:51 -0600

    001-Register 	- Added cors to allow communication with frontend
    Signed-off-by: Vianey Nava <vianey.nava@bringitps.com>
    
    	modified:   src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Enable CORS [Allow communication with frontend]
+  app.enableCors({ 
+    origin: 'http://localhost:5173', // URL de tu frontend
+    allowedHeaders: 'Content-Type, Authorization', // Permitir encabezados específicos
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
+  });
   await app.listen(3000);
 }
 bootstrap();
