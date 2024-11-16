407e69f5e36600c87e402b83f7e59143c8ee1044
Uriel Garcia <urielgarciarj@gmail.com>
2024-11-15 20:18:45 -0600

    Posts resource created
    Signed-off-by: Uriel Garcia <urielgarciarj@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,7 @@ import { ProfilesModule } from './profiles/profiles.module';
 import { UniversitiesModule } from './universities/universities.module';
 import { CompaniesModule } from './companies/companies.module';
 import { VacanciesModule } from './vacancies/vacancies.module';
+import { PostsModule } from './posts/posts.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
@@ -29,7 +30,8 @@ import { VacanciesModule } from './vacancies/vacancies.module';
     ExperiencesModule,
     UniversitiesModule,
     CompaniesModule,
-    VacanciesModule],
+    VacanciesModule,
+    PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
