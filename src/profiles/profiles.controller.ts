184f11f4e0d939936fce6acfe9cf7e596548628f
Uriel Garcia <urielgarciarj@gmail.com>
2024-12-22 23:48:33 -0600

    Endpoint upload-profile-picture added
    Signed-off-by: Uriel Garcia <urielgarciarj@gmail.com>

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Body, Param, Patch } from '@nestjs/common';
+import { Controller, Get, Body, Param, Patch, Post, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { ProfilesService } from './profiles.service';
 import { UpdateProfileDto } from './dto/update-profile.dto';
+import * as multer from 'multer';
 
 @Controller('profiles')
 export class ProfilesController {
@@ -14,9 +16,22 @@ export class ProfilesController {
     return this.profilesService.updateOrCreateProfile(userId, updateProfileDto);
   }
 
+  // Upload a post image
+  @Post('upload-profile-picture')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: multer.memoryStorage(), // Almacena en memoria para enviarlo a S3
+      limits: { fileSize: 5 * 1024 * 1024 }, // Límite de 5MB
+    }),
+  )
+  async uploadProfilePicture(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
+    const fileUrl = await this.profilesService.uploadFile(file);
+    return { message: 'File uploaded successfully', fileUrl };
+  }
+
   @Get(':id')
   async findOne(@Param('id') id: string) {
     return this.profilesService.findOne(Number(id));
   }
-
 }
--- src/profiles/profiles.service.ts
+++ src/profiles/profiles.service.ts
@@ -1,17 +1,27 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException, HttpException, HttpStatus } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Profile } from './profile.entity';
 import { UpdateProfileDto } from './dto/update-profile.dto';
 import { User } from 'src/users/user.entity';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 
 @Injectable()
 export class ProfilesService {
-
+  private s3Client: S3Client;
+  private bucketName = process.env.AWS_BUCKET_NAME;
   constructor(
     @InjectRepository(Profile) private profileRepository: Repository<Profile>,
     @InjectRepository(User) private userRepository: Repository<User>,
-  ) {}
+  ) {
+    this.s3Client = new S3Client({
+      region: process.env.AWS_REGION,
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      },
+    });
+  }
   
   async updateOrCreateProfile(userId: number, updateProfileDto: UpdateProfileDto): Promise<Profile> {
     // Look for the user
@@ -32,6 +42,31 @@ export class ProfilesService {
     return this.profileRepository.save(profile);
   }
 
+  // Upload a profile image
+  async uploadFile(file: Express.Multer.File): Promise<string> {
+    if (!file) {
+      throw new HttpException('File is not provided', HttpStatus.BAD_REQUEST);
+    }
+
+    const fileName = `${Date.now()}_${file.originalname}`;
+    const params = {
+      Bucket: this.bucketName,
+      Key: fileName,
+      Body: file.buffer,
+      ContentType: file.mimetype,
+    };
+
+    try {
+      await this.s3Client.send(new PutObjectCommand(params));
+      return `https://${this.bucketName}.s3.${process.env.AWS_REGION}.amazonaws.com/${fileName}`;
+    } catch (error) {
+      throw new HttpException(
+        'Failed to upload file',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
   async findOne(id: number): Promise<Profile> {
     const profile = await this.profileRepository.findOne({ where: { id } });
     if (!profile) {
@@ -39,4 +74,4 @@ export class ProfilesService {
     }
     return profile;
   }
-} 
+}
